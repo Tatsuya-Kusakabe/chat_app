7d6d4950ab1f2b3f2adfe5ed052d66cf03a59b6f
Tatsuya <user@USERnoMacBook-Pro.local>
2018-12-04 23:29:51 +0900

    Heroku deploy incomplete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  #
+  # ** https://github.com/browserify-rails/browserify-rails/issues/167
+  #
+  config.browserify_rails.paths << Rails.root.join('../../node_modules')
   # Code is not reloaded between requests.
   config.cache_classes = true
 
