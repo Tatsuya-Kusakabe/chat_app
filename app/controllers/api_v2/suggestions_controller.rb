a0599894c7d1fd6ff9331bad2ea120135eb3a4bc
Tatsuya <user@USERnoMacBook-Pro.local>
2018-12-15 23:34:59 +0900

    Make ApiV2::RelationshipsController and add create, destroy

--- app/controllers/api_v2/suggestions_controller.rb
+++ app/controllers/api_v2/suggestions_controller.rb
@@ -4,8 +4,8 @@ class ApiV2::SuggestionsController < ApplicationController
   # Syntax ... https://teratail.com/questions/97764
 
   def index
-    friends = User.where("id NOT IN (?)", friends_id)
-    render(json: friends)
+    suggestions = User.where("id NOT IN (?)", friends_id)
+    render(json: suggestions)
   end
 
 end
