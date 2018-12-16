52cb0275d6acfdf3c71a0564f297915ff9982dbe
Tatsuya <user@USERnoMacBook-Pro.local>
2018-12-16 15:56:51 +0900

    Modify ApiV2::FriendsController and ApiV2::SuggestionsController

--- app/controllers/api_v2/friends_controller.rb
+++ app/controllers/api_v2/friends_controller.rb
@@ -2,11 +2,21 @@
 class ApiV2::FriendsController < ApplicationController
 
   # Newly created
-  # ** Syntax ... https://teratail.com/questions/97764
-
   def index
-    friends = User.where("id IN (?)", friends_id)
+
+    # Extracting users using 'friends_id'
+    # ** https://teratail.com/questions/97764
+    friends = User.where(
+      "id IN (?)", friends_id
+
+    # Limiting columns for quicker data loading
+    ).select(
+      'id, name, profile_picture, profile_comment, status, read, email'
+    )
+
+    # Returning 'friends'
     render(json: friends)
+
   end
 
 end
--- app/controllers/api_v2/suggestions_controller.rb
+++ app/controllers/api_v2/suggestions_controller.rb
@@ -2,11 +2,21 @@
 class ApiV2::SuggestionsController < ApplicationController
 
   # Newly created
-  # ** Syntax ... https://teratail.com/questions/97764
-
   def index
-    suggestions = User.where("id NOT IN (?)", friends_id)
+
+    # Extracting suggestions using 'friends_id'
+    # ** https://teratail.com/questions/97764
+    suggestions = User.where(
+      "id NOT IN (?)", friends_id
+
+    # Limiting columns for quicker data loading
+    ).select(
+      'id, name, profile_picture, profile_comment, status, read, email'
+    )
+
+    # Returning 'suggestions'
     render(json: suggestions)
+    
   end
 
 end
