c212b18d80069704291ad4d420cbfb57468d60c5
Tatsuya-Kusakabe <44537674+Tatsuya-Kusakabe@users.noreply.github.com>
2018-12-21 13:18:59 +0900

    Revert "Make 'ApiV2::SuggestionsController' and add 'index'"

--- app/controllers/api_v2/friends_controller.rb
+++ app/controllers/api_v2/friends_controller.rb
@@ -1,10 +1,8 @@
 
 class ApiV2::FriendsController < ApplicationController
 
-  # Syntax ... https://teratail.com/questions/97764
-
   def index
-    friends = User.where("id IN (?)", friends_id)
+    friends = User.where(id: friends_id)
     render(json: friends)
   end
 
